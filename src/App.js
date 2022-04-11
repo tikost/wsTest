8fa6144b5a2955932af2a9857872b6013a059b25
tikost <tijanakostovic@hotmail.com>
2022-04-11 15:11:20 +0200

    handshake works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button} from "./Button";
+import Stomp from "stompjs";
+//import socket from "sockets";
+import SockJS from "sockjs-client";
+import {over} from 'stompjs';
+
+
+
+//var url = "ws://localhost:15674/ws";
+//var client = Stomp.client(url);
+var stompClient = null;
+
+function connect() {
+    var socket1 = new SockJS('http://localhost:8082/gs-guide-websocket');
+    stompClient = Stomp.over(socket1);
+    stompClient.connect({}, function(frame) {
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/greetings', function(greeting) {
+            console.log('Here');
+            console.log('Message is: ' + greeting);
+        });
+        console.log('Here2');
+    })
+}
+
+
+connect();
+
+function sendName() {
+  stompClient.send("/app/hello", {}, JSON.stringify("Tijana"));
+  /*
+  var pre = document.createElement("p");
+  pre.innerHTML = stompClient.send("/app/hello", {}, JSON.stringify("Tijana")).data;
+
+   */
+  alert('Got the greeting');
+}
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+
+          <Button
+              width="100%"
+              onClick={() => sendName()}
+          >
+            Click here for greeting
+          </Button>
+
+
+
+        </header>
+      </div>
   );
 }
 
 export default App;
+
---
+++ src/Button.js
@@ -0,0 +1,8 @@
+export const Button = props => (
+    <button
+        {...props}
+        style={{width: props.width, ...props.style}}
+        className={`primary-button ${props.className}`}>
+        {props.children}
+    </button>
+);
